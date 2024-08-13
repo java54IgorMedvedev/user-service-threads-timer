d900a9858a02c6a048d2f37d4f1d2b6607218e09
IgorMedvedev <psydov81@gmail.com>
2024-08-13 20:05:53 +0300

    hw #45

--- src/telran/multithreading/Timer.java
+++ src/telran/multithreading/Timer.java
@@ -1,21 +1,33 @@
 package telran.multithreading;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Timer extends Thread {
-	//TODO 
-	public Timer() {
+	private DateTimeFormatter timeFormat;
+	private long resolution;
+	private volatile boolean running = true;
+
+	public Timer(String timePattern, long resolutionMillis) {
+		this.timeFormat = DateTimeFormatter.ofPattern(timePattern);
+		this.resolution = resolutionMillis;
 		setDaemon(true);
 	}
+
+	@Override
 	public void run() {
-		while(true) {
-			System.out.println(LocalTime.now());
+		while (running) {
+			System.out.println(LocalTime.now().format(timeFormat));
 			try {
-				sleep(1000);
+				sleep(resolution);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				Thread.currentThread().interrupt();
+				running = false; 
 			}
 		}
+	}
+
+	public void stopTimer() {
+		running = false;
 	}
 }
--- src/telran/multithreading/TimerControllerAppl.java
+++ src/telran/multithreading/TimerControllerAppl.java
@@ -3,10 +3,10 @@ package telran.multithreading;
 public class TimerControllerAppl {
 
 	public static void main(String[] args) throws InterruptedException {
-		Timer timer = new Timer();
+		Timer timer = new Timer("HH:mm:ss", 1000);
 		timer.start();
-		Thread.sleep(10000);
-		//TODO
+		Thread.sleep(5000);
+		timer.stopTimer(); 		
+		Thread.sleep(5000);
 	}
-
 }
